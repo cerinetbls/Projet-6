e9037e854a228c777626a24355194e947c223538
cerinetbls <cerine.tbls@gmail.com>
2024-07-13 22:10:01 +0200

    Ajout de middleware CORS et express.json

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,27 @@
-const express = require('express');
-
 const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
 });
-
 app.use((req, res, next) => {
   res.status(201);
   next();
 });
-
 app.use((req, res, next) => {
   res.json({ message: 'Votre requête a bien été reçue !' });
   next();
 });
-
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
 });
-
-module.exports = app;
+module.exports = app; 
