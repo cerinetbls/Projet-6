1e7f778cb3b3684b1fa442528377181616e86bee
cerinetbls <cerine.tbls@gmail.com>
2024-07-14 19:52:56 +0200

    Intégration de MongoDB avec Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+// Middleware pour parser les requêtes JSON
 app.use(express.json());
 
+// Middleware pour gérer les en-têtes CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
+// Middleware pour loguer les requêtes reçues
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    console.log('Requête reçue !');
+    next();
 });
+
+// Middleware pour définir le statut de la réponse
 app.use((req, res, next) => {
-  res.status(201);
-  next();
+    res.status(201);
+    next();
 });
+
+// Middleware pour envoyer une réponse JSON
 app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
 });
+
+// Middleware pour loguer les réponses envoyées
 app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+    console.log('Réponse envoyée avec succès !');
+    next();  // Ajout de next() pour continuer la chaîne de middleware
 });
-module.exports = app; 
+
+// Connexion à MongoDB
+mongoose.connect(
+    "mongodb+srv://cerine_tbls:Trabelsi123@cluster0.lfdsenh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(err => console.log('Connexion à MongoDB échouée :', err));
+
+module.exports = app;
