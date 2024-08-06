da7b0d522479f36fdd9481e68d566101b3d12460
cerinetbls <cerine.tbls@gmail.com>
2024-08-06 19:41:57 +0200

    Implementation de dotenv pour sécuriser les variables d'environnement

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,9 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+require('dotenv').config(); // Charger dotenv
 
-//Inscription
+// Inscription
 exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
@@ -13,43 +14,35 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé" }))
-        .catch((error) => res.status(400).json({ error }));
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error: "Erreur lors de la création de l'utilisateur" }));
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error: "Erreur interne du serveur" }));
 };
 
-//Login
+// Connexion
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
-      if (user === null) {
-        res
-          .status(401)
-          .json({ message: "Paire identifiant/Mot de passe incorrecte" });
-      } else {
-        bcrypt
-          .compare(req.body.password, user.password)
-          .then((valid) => {
-            if (!valid) {
-              res
-                .status(401)
-                .json({ message: "Paire identifiant/mot de passe incorrecte" });
-            } else {
-              res.status(200).json({
-                userId: user._id,
-                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-                  expiresIn: "24h",
-                }),
-              });
-            }
-          })
-          .catch((error) => {
-            res.status(500).json({ error });
-          });
+      if (!user) {
+        return res.status(401).json({ message: "Paire identifiant/mot de passe incorrecte" });
       }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ message: "Paire identifiant/mot de passe incorrecte" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "24h" }
+            ),
+          });
+        })
+        .catch((error) => res.status(500).json({ error: "Erreur interne du serveur" }));
     })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    .catch((error) => res.status(500).json({ error: "Erreur interne du serveur" }));
 };
